7a88ffce451f07cba590e76c0f813dc25a54d40d
dilluti0n <hskimse1@gmail.com>
2025-07-19 23:37:46 +0900

    implement: TLS clienthello parser
    filter clienthello packet

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,120 @@ use anyhow::Result;
 
 fn install_rules(ipt: &IPTables) -> Result<(), Box<dyn Error>> {
     ipt.new_chain("mangle", "DPIBREAK")?;
-    ipt.insert("mangle", "PREROUTING", "-j DPIBREAK", 1)?;
+    ipt.insert("mangle", "POSTROUTING", "-j DPIBREAK", 1)?;
     ipt.append("mangle", "DPIBREAK",
                "-p tcp --dport 443 -j NFQUEUE --queue-num 0 --queue-bypass")?;
     Ok(())
 }
 
 fn cleanup_rules(ipt: &IPTables) -> Result<(), Box<dyn Error>> {
-    _ = ipt.delete("mangle", "PREROUTING", "-j DPIBREAK");
+    _ = ipt.delete("mangle", "POSTROUTING", "-j DPIBREAK");
     _ = ipt.flush_chain("mangle", "DPIBREAK");
     _ = ipt.delete_chain("mangle", "DPIBREAK");
 
     Ok(())
 }
 
+struct TLSMsg<'a> {
+    ptr: usize,
+    payload: &'a [u8]
+}
+
+fn bytes_to_usize(bytes: &[u8], size: usize) -> Option<usize> {
+    Some(match size {
+        1 => bytes[0] as usize,
+        2 => u16::from_be_bytes(bytes.try_into().ok()?) as usize,
+        3 => {
+            ((bytes[0] as usize) << 16)
+                | ((bytes[1] as usize) << 8)
+                | (bytes[2] as usize)
+        }
+        4 => u32::from_be_bytes(bytes.try_into().ok()?) as usize,
+        8 => u64::from_be_bytes(bytes.try_into().ok()?) as usize,
+        _ => return None,
+    })
+}
+
+impl<'a> TLSMsg<'a> {
+    fn new(payload: &'a [u8]) -> Self {
+        Self { ptr: 0, payload }
+    }
+
+    fn pass(&mut self, size: usize) {
+        self.ptr += size;
+    }
+
+    fn get_bytes(&mut self, size: usize) -> Option<&'a [u8]> {
+        if size == 0 {
+            return None;
+        }
+
+        let end = self.ptr + size;
+        let ret = &self.payload[self.ptr..end];
+        self.ptr = end;
+        Some(ret)
+    }
+
+    fn get_uint(&mut self, size: usize) -> Option<usize> {
+        bytes_to_usize(self.get_bytes(size)?, size)
+    }
+
+    fn get_vector(&mut self, size: usize) -> Option<&'a [u8]> {
+        let orig_ptr = self.ptr;
+        let len_bytes = self.get_bytes(size)?;
+        let length: usize = match bytes_to_usize(len_bytes, size) {
+            Some(val) => val,
+            None => {
+                self.ptr = orig_ptr;
+                return None;
+            },
+        };
+
+        self.get_bytes(length)
+    }
+}
+
+macro_rules! try_ret {
+    ($e:expr, $ret:expr) => {
+        match $e {
+            Some(val) => val,
+            None => return $ret,
+        }
+    }
+}
+
+fn is_client_hello(payload: &[u8]) -> bool {
+    let mut handshake = TLSMsg::new({
+        let mut record = TLSMsg::new(payload);
+        _ = match record.get_uint(1) { // type
+            Some(22) => {},
+            _ => return false          // not handshake
+        };
+
+        record.pass(2);                 // legacy_record_version
+        let length: usize = try_ret!(record.get_uint(2), false);
+        let fragment: &[u8] = try_ret!(record.get_bytes(length), false);
+
+        fragment
+    });
+
+    _ = match handshake.get_uint(1) { // msg_type
+        Some(1) => {},
+        _ => return false          // not clienthello
+    };
+
+    true
+}
+
 fn handle_packet(msg: &mut nfq::Message) -> Result<()> {
-    // TODO
+    let payload = msg.get_payload();
+
+    if is_client_hello(&payload) {
+        msg.set_verdict(nfq::Verdict::Drop);
+    } else {
+        msg.set_verdict(nfq::Verdict::Accept);
+    }
+
     Ok(())
 }
 
