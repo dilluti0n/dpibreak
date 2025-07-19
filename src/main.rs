4e2da2e850c0f29191244ff09ac3c550c671bb26
dilluti0n <hskimse1@gmail.com>
2025-07-19 20:03:05 +0900

    implement: packet catch with nfq-rs
    * mock POSIX signal handling with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 use iptables::IPTables;
-use tokio::signal;
+use tokio::{signal, select};
+use std::os::unix::io::{AsRawFd, RawFd};
+use tokio::io::unix::AsyncFd;
+use anyhow::Result;
 
 fn install_rules(ipt: &IPTables) -> Result<(), Box<dyn Error>> {
     ipt.new_chain("mangle", "DPIBREAK")?;
@@ -11,21 +14,52 @@ fn install_rules(ipt: &IPTables) -> Result<(), Box<dyn Error>> {
 }
 
 fn cleanup_rules(ipt: &IPTables) -> Result<(), Box<dyn Error>> {
-    let _ = ipt.delete("mangle", "PREROUTING", "-j DPIBREAK");
-    let _ = ipt.flush_chain("mangle", "DPIBREAK");
-    let _ = ipt.delete_chain("mangle", "DPIBREAK");
+    _ = ipt.delete("mangle", "PREROUTING", "-j DPIBREAK");
+    _ = ipt.flush_chain("mangle", "DPIBREAK");
+    _ = ipt.delete_chain("mangle", "DPIBREAK");
 
+    Ok(())
+}
+
+fn handle_packet(msg: &mut nfq::Message) -> Result<()> {
+    // TODO
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let ipt = iptables::new(false)?;
-    let _ = cleanup_rules(&ipt); // in case bad exit without cleanup
+    _ = cleanup_rules(&ipt); // in case bad exit without cleanup
     install_rules(&ipt)?;
 
-    signal::ctrl_c().await?;
-    let _ = cleanup_rules(&ipt);
+    let mut q = nfq::Queue::open()?;
+    q.bind(0)?;
+    let raw_fd: RawFd = q.as_raw_fd();
+    let async_q = AsyncFd::new(raw_fd)?;
 
+    loop {
+        select! {
+            _ = signal::ctrl_c() => {
+                break;
+            }
+
+            res = async_q.readable() => {
+                _ = res?;
+                loop {
+                    match q.recv() {
+                        Ok(mut msg) => {
+                            handle_packet(&mut msg)?;
+                            q.verdict(msg)?;
+                        }
+                        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                        Err(e) => return Err(e.into()),
+                    }
+                }
+            }
+        }
+    };
+
+    q.unbind(0)?;
+    cleanup_rules(&ipt)?;
     Ok(())
 }
