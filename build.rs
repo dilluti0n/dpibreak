c80ed5d036e667d0df6ad434b2e212702e449790
dilluti0n <hskimse1@gmail.com>
2025-08-05 22:27:42 +0900

    build: add log for non-windows build

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     if target_os != "windows" {
+        eprintln!("target_os is not windows");
         return;
     }
 
