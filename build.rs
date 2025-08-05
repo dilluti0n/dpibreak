a8e26a83c6142e0db568aa1145a152d393640e17
dilluti0n <hskimse1@gmail.com>
2025-08-05 15:11:50 +0900

    build: use winres to add manifest for windows binary

---
+++ build.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os != "windows" {
+        return;
+    }
+
+    let mut res = winres::WindowsResource::new();
+
+    res.set_manifest(r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#);
+    res.compile().expect("Failed to compile manifest resource");
+}
