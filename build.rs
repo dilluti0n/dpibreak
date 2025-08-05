5905a43acfa10d79416d7cac58a1713a5f0adcd5
dilluti0n <hskimse1@gmail.com>
2025-08-05 22:23:11 +0900

    build: move windows app.manifest to res/app.manifest from build.rs

--- build.rs
+++ build.rs
@@ -6,17 +6,6 @@ fn main() {
 
     let mut res = winres::WindowsResource::new();
 
-    res.set_manifest(r#"
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">
-  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-      <requestedPrivileges>
-        <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
-      </requestedPrivileges>
-    </security>
-  </trustInfo>
-</assembly>
-"#);
+    res.set_manifest_file("res/app.manifest");
     res.compile().expect("Failed to compile manifest resource");
 }
