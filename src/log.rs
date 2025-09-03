637a5008dc8c7b9e7a5698433993e08638717297
dilluti0n <hskimse1@gmail.com>
2025-09-03 21:40:28 +0900

    Implement log.rs; log level handling

---
+++ src/log.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Fatal                       // unrecovable error
+}
+
+pub static LOG_LEVEL: LogLevel = LogLevel::Warning;
+
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let prefix = match self {
+            LogLevel::Debug   => "[DEBUG]",
+            LogLevel::Info    => "[INFO]",
+            LogLevel::Warning => "[WARNING]",
+            LogLevel::Error   => "[ERROR]",
+            LogLevel::Fatal   => "[FATAL]",
+        };
+        write!(f, "{}", prefix)
+    }
+}
+
+#[macro_export]
+macro_rules! log_println {
+    ($level:expr, $($arg:tt)*) => {{
+        if crate::log::LOG_LEVEL <= $level {
+            println!("{} {}", $level, format!($($arg)*));
+        }
+    }};
+}
