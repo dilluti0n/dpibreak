deb875aeaebc1224e0d75c0aa088213fcf3b75ab
dilluti0n <hskimse1@gmail.com>
2025-09-05 16:02:51 +0900

    Add license document and copyright notice headers

--- build.rs
+++ build.rs
@@ -1,3 +1,20 @@
+// Copyright 2025 Dillution <https://github.com/dilluti0n>.
+//
+// This file is part of DPIBreak.
+//
+// DPIBreak is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// DPIBreak is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with DPIBreak. If not, see <https://www.gnu.org/licenses/>.
+
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     if target_os != "windows" {
