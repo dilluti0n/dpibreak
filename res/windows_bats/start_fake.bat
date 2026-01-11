044276dbdc6f6ca902777d71525339a6160499a6
hskimse <hskimse1@gmail.com>
2026-01-12 03:02:13 +0900

    windows: simplify start_fake.bat
    Drop self-elevation/admin checks and interactive prompts; run dpibreak via
    `start` from the script directory to match intended UX.
    Add SPDX copyright/license header.
    
    Refs: https://github.com/dilluti0n/dpibreak/issues/9

--- res/windows_bats/start_fake.bat
+++ res/windows_bats/start_fake.bat
@@ -1,40 +1,15 @@
+REM SPDX-FileCopyrightText: 2025-2026 Dilluti0n <hskimse1@gmail.com>
+REM SPDX-License-Identifier: GPL-3.0-or-later
+
 @echo off
-setlocal EnableExtensions
 
 set "ARGS=--fake --fake-ttl 8"
-
 set "EXE=%~dp0dpibreak.exe"
 set "BAT=%0"
 
-if not exist "%EXE%" (
-  echo [ERROR] "%EXE%" not found.
-  pause
-  exit /b 1
-)
-
-net session >nul 2>&1
-if %errorlevel%==0 goto :run
-
-echo Requesting administrator privileges...
-powershell -NoProfile -ExecutionPolicy Bypass ^
-  -Command "Start-Process -FilePath '%~f0' -ArgumentList 'ELEVATED' -Verb RunAs"
-exit /b
-
-:run
-
-rem ===Admin===
-
-if /I "%~1"=="ELEVATED" shift
-
-echo Running: "%EXE%" %ARGS%
-echo Note: If you want to change args (like --fake-ttl) modify ARGS= line of %BAT%.
-echo(
-
 pushd "%~dp0"
-"%EXE%" %ARGS%
+start "" %EXE% %ARGS%
 set "RC=%ERRORLEVEL%"
 popd
 
-echo Exit code: %RC%
-pause
 exit /b %RC%
